0f4c378a8a3eac83c98f7027f674d5aa8e306bc4
climax-solution <blocklimax@gmail.com>
2024-02-03 00:21:20 +0100

    update card board

--- src/components/CardBoard/index.jsx
+++ src/components/CardBoard/index.jsx
@@ -1,14 +1,17 @@
+import { useEffect, useState } from "react";
 import StatusCard from "../Cards/StatusCard";
+import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
-const cards = [
+const cardDefault = [
     {
         label: "Won", bg: "bg-cyan-400", qty: 0, icon: "/assets/images/icons/spin.svg"
     },
     {
-        label: "Sign Up Bonus", bg: "bg-green-400", qty: 120, icon: "/assets/images/icons/login.svg"
+        label: "Sign Up Bonus", bg: "bg-green-400", qty: 100, icon: "/assets/images/icons/login.svg"
     },
     {
-        label: "signed up from your link", bg: "bg-red-400", qty: 130, icon: "/assets/images/icons/users.svg"
+        label: "signed up from your link", bg: "bg-red-400", qty: 0, icon: "/assets/images/icons/users.svg"
     },
     {
         label: "Total earned", bg: "bg-amber-400", qty: 0, icon: "/assets/images/icons/dollar.svg"
@@ -16,8 +19,36 @@ const cards = [
 ];
 
 const CardBoard = () => {
+
+    const navigate = useNavigate();
+    const [cards, setCards] = useState(cardDefault);
+
+    useEffect(() => {
+
+        const api = process.env.REACT_APP_API + "/profile";
+        const yourJWTToken = window.localStorage.getItem("token");
+        
+        axios.post(api,
+            {},
+            {
+                headers: {
+                    Authorization: "Bearer " + yourJWTToken
+                }
+            }
+        ).then(res => {
+            const { balance } = res.data.data
+            let _cards = [...cards];
+            _cards[3].qty = balance;
+            setCards(_cards);
+        }).catch(err => {
+            localStorage.removeItem("token");
+            navigate("/");
+        });
+
+    }, []);
+
     return (
-        <div className="grid gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1">
+        <div className="grid gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 mt-4">
             {
                 cards.map((card, index) => (
                     <StatusCard {...card} key={index} />
